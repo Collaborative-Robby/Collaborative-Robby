f1e7eae93a2d96e7ad0a44b7fb48f06761d9fb38
Davide Berardi <berardi.dav@gmail.com>
2015-08-02 16:26:11 +0000

    First experimental model

---
+++ src/robby.c
@@ -0,0 +1,227 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <dlfcn.h>
+
+enum types {
+	CAN = 0,
+	ROBBY,
+	WALL
+};
+
+struct map {
+	long unsigned int sizex, sizey;
+	long unsigned int n_robots;
+	long unsigned int n_cans;
+	void ***innermatrix;
+	struct robby **rl;
+};
+
+/* Common structure to all elements */
+/* Extendible, so we can declare beacons or some new elements */
+struct melement {
+	int type;
+};
+
+struct robby {
+	int type;
+	long unsigned int id;
+	long unsigned int x, y;
+	void (*move)(struct map *, struct robby *);
+};
+
+struct can {
+	int type;
+};
+
+/* callbacks functions */
+void (*move_callback)(struct map *, struct robby *);
+
+int map_constructor(struct map *m, long unsigned int x, long unsigned int y,
+                   long unsigned int robbynum,
+                   long unsigned int cannum)
+{
+	int i;
+
+	m->innermatrix = calloc(x, sizeof(void **));
+
+	if (!m->innermatrix)
+		return -1;
+
+	m->rl = calloc(robbynum, sizeof(struct robby));
+	if (!m->rl)
+		return -1;
+
+	for (i = 0; i < x; i++) {
+		m->innermatrix[i] = calloc(y, sizeof(void *));
+		if (!m->innermatrix[i])
+			return -1;
+		memset(m->innermatrix[i], 0, y * sizeof(void *));
+	}
+
+	m->sizex = x;
+	m->sizey = y;
+
+	m->n_robots = 0;
+	m->n_cans = cannum;
+
+	for (i = 0; i < cannum; i++) {
+		long unsigned int nx, ny;
+		struct can *c;
+		do {
+			nx = (long unsigned int) random() % x;
+			ny = (long unsigned int) random() % y;
+		} while (m->innermatrix[nx][ny]);
+		c = malloc(sizeof(struct can));
+		if (!c)
+			return -1;
+		c->type=CAN;
+		m->innermatrix[nx][ny] = c;
+	}
+
+	return 0;
+}
+
+void map_destructor(struct map *m)
+{
+	int i;
+	if (!m)
+		return;
+	for (i = 0; i < m->sizey; i++)
+		free(m->innermatrix[i]);
+	free(m->innermatrix);
+}
+
+#define print_dispatcher(m, i, j) ({\
+	struct melement *e;\
+	if (!m->innermatrix[i][j])\
+		printf("  ");\
+	else{\
+		e = (struct melement *) m->innermatrix[i][j];\
+		if (e->type == ROBBY)\
+			printf("R%lu",((struct robby *)e)->id);\
+		else\
+			printf(" %d",e->type);\
+	}\
+})
+
+static inline void print_map(struct map *m)
+{
+	int i, j;
+	if (!m || !m->innermatrix)
+		return;
+
+	for (i = 0; i < m->sizex; i++) {
+		printf("[ ");
+		for (j = 0; j < m->sizey; j++) {
+			printf(" ");
+			print_dispatcher(m, i, j);
+			printf(" ");
+		}
+		printf(" ]\n");
+	}
+}
+
+struct robby *robby_constructor(struct map *m)
+{
+	struct robby *r = malloc(sizeof(struct robby));
+	if (!r)
+		return r;
+
+	r->type = ROBBY;
+	r->id = m->n_robots++;
+
+	do {
+		r->x = (long unsigned int) random() % m->sizex;
+		r->y = (long unsigned int) random() % m->sizey;
+	} while (m->innermatrix[r->x][r->y]);
+
+
+	m->innermatrix[r->x][r->y] = r;
+	m->rl[m->n_robots - 1] = r;
+
+	r->move = move_callback;
+
+	return r;
+}
+
+/* TODO windows version */
+#define RANDOM_SEED() ({\
+	unsigned int seed;\
+	int sf = open("/dev/urandom", O_RDONLY);\
+	if (read(sf, &seed, sizeof(seed)) < 0)\
+		fprintf(stderr, "error in reading random seed");\
+	srand(seed);\
+})
+
+#define print_status(m, r) ({\
+	printf("--> %lu %lu | round %lu: %lu robby %lu cans\n",\
+               m.sizex, m.sizey, r, m.n_robots, m.n_cans);\
+})
+
+#define MOVE_ALL_ROBBIES(m) ({\
+	int i;\
+	for (i=0; i < m.n_robots; i++) {\
+		m.rl[i]->move(&m, m.rl[i]);\
+	}\
+})
+
+void load_plugin(char *path)
+{
+	void *callbacks = dlopen(path, RTLD_LAZY);
+	if (!callbacks) {
+		fprintf(stderr, "%s\n", dlerror());
+		exit(EXIT_FAILURE);
+	}
+	move_callback = dlsym(callbacks, "move");
+	if (!move_callback) {
+		fprintf(stderr, "%s\n", dlerror());
+		exit(EXIT_FAILURE);
+	}
+	dlclose(callbacks);
+}
+
+int main(int argc, char **argv)
+{
+	long unsigned int sizex, sizey, robbynum, cannum, totalrounds, round;
+	struct map m;
+	int i;
+
+	if (argc < 3) {
+		return EXIT_FAILURE;
+	}
+
+	RANDOM_SEED();
+
+	/* Usage / getargs? */
+	load_plugin(argv[1]);
+
+	sizex = strtoul(argv[2], NULL, 10);
+	sizey = strtoul(argv[3], NULL, 10);
+	robbynum = strtoul(argv[4], NULL, 10);
+
+	cannum = strtoul(argv[5], NULL, 10);
+	totalrounds = strtoul(argv[6], NULL, 10);
+
+	round = 0;
+
+	if (map_constructor(&m, sizex, sizey, robbynum, cannum) != 0) {
+		perror("map construction");
+		return EXIT_FAILURE;
+	}
+
+	for (i = 0; i < robbynum; i++) {
+		robby_constructor(&m);
+	}
+
+	for (round = 0; round < totalrounds; round++) {
+		print_status(m, round);
+		print_map(&m);
+		MOVE_ALL_ROBBIES(m);
+	}
+
+	map_destructor(&m);
+
+}
