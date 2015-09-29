1ad063e46ed014b5ba4a3a85badd9eabfd175dee
Michele Corazza <ashmikuz@gmail.com>
2015-09-29 10:10:34 +0200

    plot script orientation

--- tools/plot.sh
+++ tools/plot.sh
@@ -5,4 +5,4 @@ GENCOUNT=$(tail -4 ../build/fitvalues | head -1 | cut -d " " -f 3)
 #X=$(sed -n "/g$I\$/,/g$(($I+1))\$/p" fitvalues)
 #X=$(echo "$X" | tail -n +2 | head -n -1)
 
-(echo "splot for [i=0:$GENCOUNT] \"../build/fitvalues\" index i  using 1:3:2 with pm3d notitle";cat) | gnuplot;
+(echo "set ticslevel 0;set view 60, 120, 1, 1 ;splot for [i=0:$GENCOUNT] \"../build/fitvalues\" index i  using 1:3:2 with pm3d notitle";cat) | gnuplot;
