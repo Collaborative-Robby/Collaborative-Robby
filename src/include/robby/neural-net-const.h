e01ebaf449091cab28147dd04da0722a3df64e91
Davide Berardi <berardi.dav@gmail.com>
2015-10-22 12:56:51 +0200

    Constant values of the paper.

--- src/include/robby/neural-net-const.h
+++ src/include/robby/neural-net-const.h
@@ -2,10 +2,10 @@
 #define ROBBY_NEURAL_NET_CONST_H
 
 /* Neural net mutation rates */
-#define MUTATION_RATE_NODE 0.75
+#define MUTATION_RATE_NODE 0.03
 #define MUTATION_RATE_CONNECTION 0.75
-#define MUTATION_RATE_LINK 2.0
-#define MUTATION_RATE_BIAS 0.6
+#define MUTATION_RATE_LINK 0.1
+#define MUTATION_RATE_BIAS 0.1
 #define MUTATION_RATE_ENABLE 0.4
 #define MUTATION_RATE_DISABLE 0.6
 
@@ -15,14 +15,14 @@
 #define PERTURB_STEP 0.05
 
 /* Species comparison parameters */
-#define SAME_SPECIES_TRESHOLD 1.0
+#define SAME_SPECIES_TRESHOLD 3.0
 #define COEFFICIENT_DELTA_WEIGHT 0.4
-#define COEFFICIENT_EXCESS_GENES 2.0
-#define COEFFICIENT_DISJOINT_GENES 2.0
+#define COEFFICIENT_EXCESS_GENES 1.0
+#define COEFFICIENT_DISJOINT_GENES 1.0
 #define DISABLE_INHERIT_GENE_RATIO 0.75
 
 /* Number of turns of species survival. */
-#define SPECIES_STALE_TRESHOLD 100
+#define SPECIES_STALE_TRESHOLD 50
 
 /* Parameter of the sigmoid activation function */
 #define SIGMOID_BETA -4.9
