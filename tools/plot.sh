2847603bb115fe7450fe8be6bd3edf2bd9c9bd66
Michele Corazza <ashmikuz@gmail.com>
2015-12-11 10:39:09 +0100

    better plot script

--- tools/plot.sh
+++ tools/plot.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-GENCOUNT=$(tail -4 ../build/fitvalues | head -1 | cut -d " " -f 3)
 
 #X=$(sed -n "/g$I\$/,/g$(($I+1))\$/p" fitvalues)
 #X=$(echo "$X" | tail -n +2 | head -n -1)
 
-(echo "set ticslevel 0;set view 60, 120, 1, 1 ;splot for [i=0:$GENCOUNT] \"../build/fitvalues\" index i  using 1:3:2 with pm3d notitle";cat) | gnuplot;
+if [ $# -lt 1 ]; then
+    FILENAME="../build/fitvalues"
+else
+    FILENAME=$1
+fi
+
+GENCOUNT=$(tail -4 $FILENAME | head -1 | cut -d " " -f 3)
+
+(echo "set term png size 600,600; set output \"graph.png\"; set xlabel \"Numero Robby\" rotate parallel offset 3,0; set ylabel \"Generazioni\" rotate parallel offset 0,-1; set zlabel \"Fitness\" rotate parallel;\
+set ticslevel 0;set view 60, 120, 1, 1 ;splot for [i=0:$GENCOUNT] \"$FILENAME\" index i  using 1:3:2 with pm3d notitle") | gnuplot;
